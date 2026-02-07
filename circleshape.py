e15ecf48e205e0debba1b357f28a8046c493819d
Matyas Angyal <matyas.angyal@one-beyond.com>
2026-02-07 21:51:35 +0100

    fix: remove extra print

--- circleshape.py
+++ circleshape.py
@@ -22,5 +22,4 @@ class CircleShape(pygame.sprite.Sprite):
 
     def collides_with(self, other):
         distance = self.position.distance_to(other.position)
-        print(distance, self.radius, other.radius)
         return (self.radius + other.radius) >= distance
