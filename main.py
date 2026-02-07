d04b9ea19fba13023bbcd4d5c89fec2c7478204c
Matyas Angyal <matyas.angyal@one-beyond.com>
2026-02-07 10:21:35 +0100

    feat: add event loop basic display

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            pass
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
